1d82b6bee876a6008d8ce01a768debe56b6ad700
Manjur Ahmed <Manjur Ahmed>
2023-01-13 12:52:28 -0500

    add configs

--- apps/two-step-api/src/app/app.module.ts
+++ apps/two-step-api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { CoreModule } from '@two-step/core'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
